d02aa16f095233d3277d1843b2e89bf0c346975c
emil <emil@njord>
2010-02-12 13:05:25 -0800

    Added a first example for how to write a specification.
    A first executable computing and displaying a simple Grobner basis is
    written. This code relies on none of Jason's specification work, but
    rather treats the library as just an API for Grobner basis computations.

---
+++ examples/First.hs
@@ -0,0 +1,16 @@
+import Math.GrobnerPar
+import Math.GrobnerPar.Polynomial
+import Math.GrobnerPar.Monomial
+import Math.GrobnerPar.Buchberger
+
+a = monomialTerm (om (M [1,0,0])) :: Polynomial Rational DegRevLex
+b = monomialTerm (om (M [0,1,0])) :: Polynomial Rational DegRevLex
+c = monomialTerm (om (M [0,0,1])) :: Polynomial Rational DegRevLex
+
+g1 = b^2-a*c
+g2 = a^3-c^2
+
+gens = [g1,g2]
+
+main = do
+       putStrLn $ unlines $ map show $ grobnerBasis gens
