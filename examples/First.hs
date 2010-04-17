fe9e663ff1e07427c288856f54543c27610b956f
Mikael Vejdemo Johansson <mik@math.stanford.edu>
2010-04-16 20:53:31 -0700

    Vectorized

--- examples/First.hs
+++ examples/First.hs
@@ -3,9 +3,7 @@ import Math.GrobnerPar.Polynomial
 import Math.GrobnerPar.Monomial
 import Math.GrobnerPar.Buchberger
 
-a = monomialTerm (om (M [1,0,0])) :: Polynomial Rational DegRevLex
-b = monomialTerm (om (M [0,1,0])) :: Polynomial Rational DegRevLex
-c = monomialTerm (om (M [0,0,1])) :: Polynomial Rational DegRevLex
+[a,b,c] = nVars 3 :: [Polynomial Rational DegRevLex]
 
 g1 = b^2-a*c
 g2 = a^3-c^2
