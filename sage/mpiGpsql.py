afbd5f54b981814778dd0b755445dc7e23d97c6f
Mikael Vejdemo Johansson <mik@math.stanford.edu>
2010-04-20 14:35:09 -0700

    SQLAlchemy storage engine in place.

--- sage/mpiGpsql.py
+++ sage/mpiGpsql.py
@@ -1,19 +1,39 @@
 #!/usr/bin/env sage -python
 
-import mgGrobner
+from mgGrobner import *
+from sqlalchemy import *
 
-class pgsql:
+class sql:
     def __init__(self):
-        self.conn = None
+        self.engine = create_engine("sqlite:////tmp/mpigrobner.db", echo=False)
+        self.metadata = MetaData()
+        self.stable = Table('stable', self.metadata, 
+                       Column('id', Integer, primary_key=True),
+                       Column('degree', String(100), nullable=False), 
+                       Column('poly', PickleType(),nullable=False))
+        self.new = Table('new', self.metadata, 
+                    Column('id', Integer, primary_key=True),
+                    Column('degree', String(100),nullable=False), 
+                    Column('poly', PickleType(),nullable=False))
+        self.metadata.create_all(self.engine)
+        self.metadata.bind = self.engine
 
     def storeStable(self,fs):
-        pass
+        inserts = map(lambda f: {'degree': str(multidegree(f)), 'poly': f},fs)
+        self.stable.insert().execute(inserts)
 
     def storeNew(self, fs):
-        pass
+        inserts = map(lambda f: {'degree': str(multidegree(f)), 'poly': f},fs)
+        self.new.insert().execute(inserts)
+
 
     def loadStable(self,deg):
-        return []
+        select = self.stable.select()
+        select.where(self.stable.c.degree == str(deg))
+        selects = select.execute()
+        ret = [s[self.stable.c.poly] for s in selects]
+        selects.close()
+        return ret
 
     def loadStableBelow(self,deg):
         ret = []
@@ -23,4 +43,10 @@ class pgsql:
         return ret
 
     def loadNew(self,deg):
-        return []
+        select = self.new.select([])
+        select.where(self.new.c.degree == str(deg))
+        selects = select.execute()
+        ret = [s[self.new.c.poly] for s in selects]
+        selects.close()
+        return ret
+
