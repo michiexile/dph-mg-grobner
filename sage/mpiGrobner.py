1b912ad4fc3aa339a56b5e41127de82baa0d4bfa
Mikael Vejdemo Johansson <mik@math.stanford.edu>
2010-04-20 14:35:09 -0700

    SQLAlchemy storage engine in place.

--- sage/mpiGrobner.py
+++ sage/mpiGrobner.py
@@ -4,6 +4,7 @@ from sage.all import *
 from mpi4py import MPI
 from mgGrobner import *
 from collections import defaultdict
+from mpiGpsql import *
 
 class grobner:
     def __init__(self, gens):
@@ -11,12 +12,26 @@ class grobner:
         self.gens = gens
         self.stable = defaultdict(dict)
         self.new = defaultdict(dict)
+        self.sql = None
 
     def node(self):
         print "I'm a node! I'm number %d!" % self.comm.Get_rank()
+        self.comm.Barrier()
+        self.sql=sql()
+        gens11 = self.sql.loadNew((1,1))
+        print gens11 
+        for f in gens11:
+            if f.lm() not in self.new[multidegree(f)].keys():
+                self.new[multidegree(f)][f.lm()]=[]
+            self.new[multidegree(f)][f.lm()].append(f)
+        print "Node accessed the new things"
+        print self.new
 
     def control(self):
         print "I'm control. We'll deal with: %s" % repr(self.gens)
+        self.sql=sql()
+        self.comm.Barrier()
+        self.sql.storeNew(self.gens)
         for g in self.gens:
             if g.lm() not in self.new[multidegree(g)].keys():
                 self.new[multidegree(g)][g.lm()]=[]
